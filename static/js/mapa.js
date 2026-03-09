a0a43f09f7cd7d7bfcb9630d70fa86ea58c50395
Kelly Herrera <kelly.herrera@ug.uchile.cl>
2026-03-09 17:03:10 -0300

    style: new popup for orders and donations

--- static/js/mapa.js
+++ static/js/mapa.js
@@ -1,4 +1,7 @@
-let map = L.map("map").setView([-33.457925, -70.664511], 7);
+// -----------------------------
+// Map initialization
+// -----------------------------
+const map = L.map("map").setView([-33.457925, -70.664511], 7);
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -6,74 +9,117 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+
+// -----------------------------
+// Marker utility
+// -----------------------------
+function createMarker(lat, lng, color, popupContent) {
+
+  const marker = L.circleMarker([lat, lng], {
+    radius: 8,
+    color: color,
+    fillColor: color,
+    fillOpacity: 0.9
+  }).addTo(map);
+
+  marker.bindPopup(popupContent);
+}
+
+
+// -----------------------------
+// Coordinate offset logic
+// prevents markers overlapping
+// -----------------------------
+function adjustCoordinates(lat, lng, commune, communeTracker){
+
+  if (commune in communeTracker) {
+
+    const offset = communeTracker[commune];
+
+    lat = lat - offset * 0.001;
+    lng = lng - offset * 0.0000005;
+
+    communeTracker[commune] += 1;
+
+  } else {
+    communeTracker[commune] = 1;
+  }
+
+  return [lat, lng];
+}
+
+
+// -----------------------------
+// Fetch map data
+// -----------------------------
 fetch("http://localhost:5000/obtener-mapa")
-  .then((response) => response.json())
-  .then((data) => {
-    const comunas = {};
-    let pedidos = data[0]
-    let donaciones = data[1]
-    let i = 0;
+  .then(response => response.json())
+  .then(data => {
 
-    for(let ped of pedidos){
-      let lat_p = ped["lat_pedido"];
-      let lng_p = ped["lng_pedido"];
-      let comuna_p = ped["comuna_pedido"];
+    const orders = data[0];
+    const donations = data[1];
 
-      console.log(comunas)
+    const communeTracker = {};
 
-      //si una comuna ya está se le ajustan un poco las coordenadas para que se puedan distinguir los pedidos/donaciones.
-      if(comuna_p in comunas) {
-        lat_p = String(parseFloat(lat_p) - comunas[comuna_p]*0.001);
-        lng_p = String(parseFloat(lng_p) - comunas[comuna_p]*0.0000005);
-        comunas[comuna_p] += 1;
-      } else {
-        comunas[comuna_p] = 1;
-      }
+    renderOrders(orders, communeTracker);
+    renderDonations(donations, communeTracker);
 
-      const onMarkerClickPedido = (e) => {
-        L.popup()
-          .setLatLng([lat_p, lng_p])
-          .setContent(
-            `<h1>ID Pedido: ${ped["id_pedido"]}</h1><ul><li>Tipo: ${ped["tipo_pedido"]}</li><li>Cantidad: ${ped["cantidad_pedido"]}</li><li>Email Solicitante: ${ped["email_pedido"]}</li></ui>`
-          )
-          .openOn(map);
-      };
-
-      let marker = L.marker([lat_p, lng_p]).addTo(map);
-      marker.on("click", onMarkerClickPedido);
-    };
-    
-    for(let don of donaciones){
-      let lat_d = don["lat_donacion"];
-      let lng_d = don["lng_donacion"];
-      let comuna_d = don["comuna_donacion"];
-
-      //si una comuna ya está se le ajustan un poco las coordenadas para que se puedan distinguir los pedidos/donaciones.
-      if(comuna_d in comunas) {
-        lat_d = String(parseFloat(lat_d) - comunas[comuna_d]*0.001);
-        lng_d = String(parseFloat(lng_d) - comunas[comuna_d]*0.0000005);
-        comunas[comuna_d] +=1;
-      } else {
-        comunas[comuna_d] = 1;
-      }
-
-      const onMarkerClickDonacion = (e) => {
-        L.popup()
-          .setLatLng([lat_d, lng_d])
-          .setContent(
-            `<h1>ID Donacion: ${don["id_donacion"]}</h1><ul><li>Tipo: ${don["tipo_donacion"]}</li><li>Cantidad: ${don["cantidad_donacion"]}</li><li>Email Solicitante: ${don["email_donacion"]}</li><li>Fecha Disponibilidad: ${don["fecha_donacion"]}</li><li>Calle y Número: ${don["calle_numero"]}</li></ui>`
-          )
-          .openOn(map);
-      };
-
-      let markerd = L.marker([lat_d, lng_d],{
-        icon: L.icon({
-          iconUrl: "../static/img/green-icon.png",
-          iconSize : [30, 50],
-          iconAnchor: [20, 41],
-        })
-      }).addTo(map);
-
-      markerd.on("click", onMarkerClickDonacion);
-    };
   });
+
+
+// -----------------------------
+// Render orders
+// -----------------------------
+function renderOrders(orders, communeTracker){
+
+  for (const ord of orders){
+
+    let lat = parseFloat(ord["lat_pedido"]);
+    let lng = parseFloat(ord["lng_pedido"]);
+    const commune = ord["comuna_pedido"];
+
+    [lat, lng] = adjustCoordinates(lat, lng, commune, communeTracker);
+
+    const popupContent = `
+      <h4>ID Pedido: ${ord["id_pedido"]}</h4>
+      <ul>
+        <li><b>Tipo:</b> ${ord["tipo_pedido"]}</li>
+        <li><b>Cantidad:</b> ${ord["cantidad_pedido"]}</li>
+        <li><b>Email:</b> ${ord["email_pedido"]}</li>
+      </ul>
+    `;
+
+    createMarker(lat, lng, "#FE5F55", popupContent);
+  }
+
+}
+
+
+// -----------------------------
+// Render donations
+// -----------------------------
+function renderDonations(donations, communeTracker){
+
+  for (const don of donations){
+
+    let lat = parseFloat(don["lat_donacion"]);
+    let lng = parseFloat(don["lng_donacion"]);
+    const commune = don["comuna_donacion"];
+
+    [lat, lng] = adjustCoordinates(lat, lng, commune, communeTracker);
+
+    const popupContent = `
+      <h4>ID Donación: ${don["id_donacion"]}</h4>
+      <ul>
+        <li><b>Tipo:</b> ${don["tipo_donacion"]}</li>
+        <li><b>Cantidad:</b> ${don["cantidad_donacion"]}</li>
+        <li><b>Email:</b> ${don["email_donacion"]}</li>
+        <li><b>Fecha disponibilidad:</b> ${don["fecha_donacion"]}</li>
+        <li><b>Dirección:</b> ${don["calle_numero"]}</li>
+      </ul>
+    `;
+
+    createMarker(lat, lng, "#94C9A9", popupContent);
+  }
+
+}
